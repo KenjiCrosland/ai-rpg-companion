58987511b0fa6c56ad72c880f911443be047dda2
Kenji Crosland <kenji.crosland@gmail.com>
2024-05-01 11:49:22 -0500

    Before complete setting refactor

--- src/components/TownKingdomGenerator.vue
+++ src/components/TownKingdomGenerator.vue
@@ -1,34 +1,312 @@
 <template>
   <div class="main-content">
-    <h1>The Town of Silvervale</h1>
-    <div class="read-aloud">
-      The Town of Silvervale stands at the edge of the Forgotten Kingdom, its cobbled streets winding between towering
-      stone structures adorned with intricate clockwork mechanisms that chime melodically throughout the day. A thick
-      mist of shimmering blue energy envelops the town, crackling with static electricity and casting a soft,
-      otherworldly glow over its inhabitants and visitors alike. Crowds of townsfolk and travelers pass through the
-      shimmering gates marked with runes of protection, each visitor experiencing a tingling sensation as they cross the
-      threshold into Silvervale's unique border town. At the center of the town square, a massive sculpture crafted
-      entirely of enchanted glass orbs spins gracefully, catching and refracting the ever-present energy field in a
-      mesmerizing display that captivates onlookers and invokes a sense of wonder and curiosity. As the players explore,
-      they come across a trio of musicians performing a haunting melody on instruments made of shimmering crystal, their
-      music harmonizing with the crackling energy of the town and drawing a small crowd of mesmerized listeners.
-    </div>
+    <h1>Kingdom or Town Generator</h1>
+    <form @submit.prevent="generateTownOrKingdom()">
+      <div class="generator-fields">
+        <cdr-input class="generator-field-input" id="adjective" v-model="adjective" background="secondary"
+          label="Adjective">
+          <template #helper-text-bottom>
+            Examples: "Flourishing", "Decrepit", "Decadent"
+          </template>
+        </cdr-input>
+        <cdr-input class="generator-field-input" id="kingdomType" v-model="kingdomType" background="secondary"
+          label="Type of Place">
+          <template #helper-text-bottom>
+            Examples: "Kingdom", "Town", "City", "Republic"
+          </template>
+        </cdr-input>
+        <p>Of</p>
+        <cdr-input class="generator-field-input" id="placeName" v-model="placeName" background="secondary"
+          label="Place name">
+          <template #helper-text-bottom>
+            Examples: "Kingdomaria", "Townland", "Citytopia", "Republic of the People"
+          </template>
+        </cdr-input>
 
-    <cdr-button @click="increment">Click Me</cdr-button>
-    <p>Count: {{ count }}</p>
+      </div>
+      <cdr-input :rows="5" tag="textarea" v-model="placeLore" background="secondary" label="Setting Lore"
+        placeholder="Enter any any additional details about the setting" class="item-lore-details">
+        <template #helper-text-bottom>
+          Write any details about your setting that you want to include. Need help coming up with lore for your
+          setting? Use the <cdr-link href="https://cros.land/ai-powered-lore-and-timeline-generator/">Lore
+            Generator</cdr-link> and paste in the generated summary!
+        </template>
+      </cdr-input>
 
+      <cdr-button @click="generateTownOrKingdom">Generate</cdr-button>
+    </form>
+    <cdr-tabs v-if="kingdomOverview" height="auto" style="width: 800px">
+      <cdr-tab-panel label="Overview" name="overview">
+        <h2>{{ placeName }} Overview</h2>
+        <div>
+          <p>{{ kingdomOverview.overview }}</p>
+          <p>{{ kingdomOverview.history }}</p>
+          <p>{{ kingdomOverview.current_ruler_sentence }} {{ kingdomOverview.recent_event_current_ruler }} {{
+      kingdomOverview.recent_event_consequences }}</p>
+          <p>{{ kingdomOverview.social_history }} {{ kingdomOverview.recent_event_social }}</p>
+          <p>{{ kingdomOverview.economic_history }} {{ kingdomOverview.impactful_economic_event }}</p>
+          <p>{{ kingdomOverview.military_history }} {{ kingdomOverview.recent_event_military }}</p>
+          <p>{{ kingdomOverview.greatest_hope }}</p>
+          <p>{{ kingdomOverview.darkest_fear }}</p>
+        </div>
+      </cdr-tab-panel>
+      <cdr-tab-panel label="Important Locations" name="locations" @tab-change="generateSubLocations">
+        <h2>Important Locations</h2>
+        <div v-if="importantLocations.length > 0">
+          <cdr-list>
+            <li v-for="location in importantLocations" :key="location.name">
+              <h3>{{ location.name }}</h3>
+              <p>{{ location.description }}</p>
+            </li>
+          </cdr-list>
+        </div>
+        <div v-else>
+          <LocationListSkeleton />
+        </div>
+      </cdr-tab-panel>
+      <cdr-tab-panel v-if="kingdomOverview" label="Factions" name="factions" @tab-change="generateFactions">
+        <h2>Factions</h2>
+        <div v-if="factions">
+          <cdr-list>
+            <li v-for="faction in factions" :key="faction.name">
+              <h3>{{ faction.name }}</h3>
+              <p><strong>Influence Level: </strong>{{ factionPowerLevels[faction.influence_level - 1] }}</p>
+              <div class="focus-text">
+                <p><strong>Faction Leader, {{ faction.faction_leader }}:</strong> {{ faction.faction_leader_description
+                  }}
+                </p>
+                <p><strong>Key Strengths: </strong> {{ faction.key_resources_and_assets }}</p>
+                <p><strong>Motto: </strong>"{{ faction.motto }}"</p>
+              </div>
+
+              <p>{{ faction.history }}</p>
+              <p>{{ faction.recent_event }} {{ faction.current_situation }}</p>
+              <p>{{ faction.rites_and_ceremonies }} {{ faction.recent_ceremony }}</p>
+              <p>{{ faction.challenge_to_power }} {{ faction.challenge_event }}</p>
+            </li>
+          </cdr-list>
+        </div>
+        <div v-else>
+          <CdrSkeleton>
+            <cdr-list>
+              <li>
+                <FactionSkeleton />
+              </li>
+              <li class="bone-list-item">
+                <FactionSkeleton />
+              </li>
+              <li class="bone-list-item">
+                <FactionSkeleton />
+              </li>
+            </cdr-list>
+          </CdrSkeleton>
+        </div>
+      </cdr-tab-panel>
+      <cdr-tab-panel v-if="npcs" label="Notable NPCs" name="npcs">
+        <div v-if="npcs">
+          <h2>Notable NPCs</h2>
+          <cdr-list>
+            <li v-for="npc in npcs" :key="npc.name">
+              <h3>{{ npc.name }}</h3>
+              <p>{{ npc.description }}</p>
+            </li>
+          </cdr-list>
+        </div>
+        <p v-else>Notable NPCs not yet generated</p>
+      </cdr-tab-panel>
+      <cdr-tab-panel label="Empty" name="empty">
+        <p>Empty tab</p>
+      </cdr-tab-panel>
+    </cdr-tabs>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { CdrInput, CdrButton, CdrText, CdrSelect, CdrCheckbox, CdrLink, CdrList, CdrSkeleton, CdrSkeletonBone, IconXSm, CdrTooltip } from "@rei/cedar";
+import { ref, computed } from 'vue'
+import { CdrInput, CdrButton, CdrText, CdrSelect, CdrTabs, CdrTabPanel, CdrCheckbox, CdrLink, CdrList, CdrSkeleton, CdrSkeletonBone, IconXSm, CdrTooltip } from "@rei/cedar";
+import { kingdomOverviewPrompt, subLocationsPrompt, factionsPrompt } from "../util/kingdom-prompts.mjs";
+import FactionSkeleton from "./skeletons/FactionSkeleton.vue";
+import LocationListSkeleton from "./skeletons/LocationListSkeleton.vue";
+import { generateGptResponse } from "../util/open-ai.mjs";
+import placeAdjectives from '../data/place-adjectives.json';
+import placeNames from '../data/place-names.json';
+import '@rei/cedar/dist/style/cdr-text.css';
+import '@rei/cedar/dist/style/cdr-link.css';
+import '@rei/cedar/dist/style/cdr-list.css';
+import '@rei/cedar/dist/style/cdr-tabs.css';
+const adjective = ref('');
+const kingdomType = ref('');
+const placeName = ref('');
+const placeLore = ref('');
+const kingdomOverview = ref(null);
+const factions = ref(null);
+const importantLocations = ref([]);
+const npcs = ref([]);
+const loadingFactions = ref(false);
+const loadingSubLocations = ref(false);
 
-const message = ref('Hello, Vue 3 + Composition API!')
-const count = ref(0)
+const npcNames = computed(() => npcs.value.map(npc => npc.name));
 
-function increment() {
-  count.value++
+//computed property that turns the kindgom overview into a human readable non-json string separated by newlines
+const kingdomOverviewText = computed(() => {
+  if (!kingdomOverview.value) {
+    return '';
+  }
+  return Object.entries(kingdomOverview.value).map(([key, value]) => {
+    if (Array.isArray(value)) {
+      return '';
+    }
+    return `${value}`;
+  }).join('\n');
+});
+
+const kingdomValidation = (jsonString) => {
+  try {
+    const jsonObj = JSON.parse(jsonString);
+    const keys = [
+      'overview',
+      'history',
+      'current_ruler_sentence',
+      'recent_event_current_ruler',
+      'recent_event_consequences',
+      'social_history',
+      'recent_event_social',
+      'economic_history',
+      'impactful_economic_event',
+      'military_history',
+      'recent_event_military',
+      'greatest_hope',
+      'darkest_fear',
+      'npc_list',
+    ];
+    const npcKeys = [
+      'name',
+      'description',
+    ];
+    for (let npc of jsonObj.npc_list) {
+      if (!npcKeys.every((key) => key in npc)) {
+        return false;
+      }
+    }
+    return keys.every((key) => key in jsonObj);
+  } catch (error) {
+    return false;
+  }
+}
+
+const factionPowerLevels = [
+  'Nonexistent',
+  'Marginal',
+  'Emerging',
+  'Moderate',
+  'Noteworthy',
+  'Influential',
+  'Powerful',
+  'Dominant',
+  'Controlling',
+  'Totalitarian'
+];
+
+const sublocationValidation = (jsonString) => {
+  console.log(jsonString);
+  try {
+    const jsonObj = JSON.parse(jsonString);
+    console.log(jsonObj);
+    const locationKeys = [
+      'name',
+      'description'
+    ];
+    for (let location of jsonObj) {
+      if (!locationKeys.every((key) => key in location)) {
+        return false;
+      }
+    }
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+const factionValidation = (jsonString) => {
+  try {
+    const jsonObj = JSON.parse(jsonString);
+    const keys = [
+      'name',
+      'history',
+      'recent_event',
+      'current_situation',
+      'motto',
+      'influence_level',
+      'faction_leader',
+      'faction_leader_description',
+      'key_resources_and_assets',
+      'rites_and_ceremonies',
+      'recent_ceremony',
+      'challenge_to_power',
+      'challenge_event'
+    ];
+    for (let faction of jsonObj) {
+      if (!keys.every((key) => key in faction)) {
+        return false;
+      }
+    }
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+const generateTownOrKingdom = async () => {
+  if (!kingdomType.value) {
+    const placeTypes = Object.keys(placeAdjectives);
+    console.log(placeTypes)
+    kingdomType.value = placeTypes[Math.floor(Math.random() * placeTypes.length)];
+  }
+  if (!adjective.value && placeAdjectives[kingdomType.value.toLowerCase()]) {
+    adjective.value = placeAdjectives[kingdomType.value][Math.floor(Math.random() * placeAdjectives[kingdomType.value.toLowerCase()].length)];
+  }
+  if (!placeName.value && placeNames[kingdomType.value.toLowerCase()]) {
+    placeName.value = placeNames[kingdomType.value.toLowerCase()][Math.floor(Math.random() * placeNames[kingdomType.value.toLowerCase()].length)];
+  }
+  const prompt = kingdomOverviewPrompt(adjective.value, kingdomType.value, placeName.value, placeLore.value);
+  try {
+    const response = await generateGptResponse(prompt, kingdomValidation, 3);
+    kingdomOverview.value = JSON.parse(response);
+    npcs.value.push(...kingdomOverview.value.npc_list);
+  } catch (error) {
+    console.error("Error generating kingdom description:", error);
+  }
+}
+
+const generateSubLocations = async () => {
+  if (!kingdomOverview.value || loadingSubLocations.value || importantLocations.value.length > 0) {
+    return;
+  }
+  loadingSubLocations.value = true;
+  const subLocationsResponse = await generateGptResponse(subLocationsPrompt(kingdomOverviewText.value), sublocationValidation, 3);
+  const subLocations = JSON.parse(subLocationsResponse);
+  importantLocations.value.push(...subLocations);
+  console.log('Generated sublocations', importantLocations.value);
+  loadingSubLocations.value = false;
+}
+
+const generateFactions = async () => {
+  if (loadingFactions.value || factions.value || !kingdomOverview.value) {
+    return;
+  }
+  loadingFactions.value = true;
+  console.log('Generating factions')
+  const factionsResponse = await generateGptResponse(factionsPrompt(kingdomOverviewText.value), factionValidation, 3);
+  factions.value = JSON.parse(factionsResponse);
+  factions.value.forEach(faction => {
+    if (!npcNames.value.includes(faction.faction_leader)) {
+      npcs.value.push({
+        name: faction.faction_leader,
+        description: faction.faction_leader_description
+      });
+    }
+  });
+  loadingFactions.value = false;
 }
 </script>
 
@@ -45,10 +323,26 @@ function increment() {
   height: 100vh;
 }
 
-.read-aloud {
+.generator-fields {
+  display: flex;
+  gap: 2rem;
+  align-items: center;
+}
+
+.focus-text {
   background-color: $cdr-color-background-secondary;
   color: $cdr-color-text-secondary;
   padding: 1rem 2rem;
   font-style: italic;
+}
+
+.skeleton-line {
+  display: flex;
+  gap: 1rem;
+  margin-top: 1rem;
+}
+
+.bone-list-item {
+  margin: 4rem 0;
 }
 </style>
--- src/util/kingdom-prompts.mjs
+++ src/util/kingdom-prompts.mjs
@@ -1,23 +1,83 @@
-export function kingdomOverviewPrompt(adjective, kingdomType) {
+export function kingdomOverviewPrompt(
+  adjective,
+  kingdomType,
+  placeName,
+  placeLore,
+) {
+  let initialSentence = `Give me a description of the ${adjective} ${kingdomType} of ${placeName}.`;
+  if (!placeName) {
+    initialSentence = `Give me a description of an ${adjective} ${kingdomType}.`;
+  }
   return `
-  Give me a description of a ${adjective} ${kingdomType}. When describing events and individuals be specific about names and locations. Each key should be a sentence or two, should be detailed and specific and should flow together to create a cohesive description.
-  Return the description in JSON format with the following keys:
+  ${initialSentence}. When describing events and individuals be specific about names and locations. Each key should be a sentence or two, should be detailed and specific and should flow together to create a cohesive description. Avoid common fantasy tropes and cliches. Temperature: 0.9
+
+  ${placeLore ? `Additional details about the setting: ${placeLore}.` : ''}
+  
+  Return the description in JSON format with the following keys. Make sure the npc_list includes all npc names mentioned in the description. The NPCs in npc_list should only be individuals and NOT organizations or groups:
   {
     overview: 'A brief overview of the ${kingdomType}, with a brief description of its current state',
     history: 'A very brief history of the ${kingdomType}, including its founding and most significant recent events',
-    recent_event_consequences: 'describe the consequences of the recent event in the ${kingdomType} described above',
     current_ruler_sentence: 'A sentence describing the ruler of the ${kingdomType}. What have they done to earn their position? What was their most recent significant action?',
-    recent_event_political: 'describe a recent political event which reflects the current state of the ${kingdomType}',
+    recent_event_current_ruler: 'describe a recent political event involving the current ruler which reflects the current state of the ${kingdomType}',
+    recent_event_consequences: 'describe the consequences of the recent political event involving the current ruler',
     social_history: 'A brief history of the social structure of the ${kingdomType}',
     recent_event_social: 'this sentence should tie into the previous sentence about social history. describe a recent social event which reflects the current state of the ${kingdomType}. Show don't tell, use examples and do not say "A recent social event was...". Be specific with names of organizations, individuals, or locations',
     economic_history: 'A brief history of the economic structure of the ${kingdomType}',
-    recent_event_economic: 'this sentence should tie into the previous sentence about economic history. describe a recent economic event which reflects the current state of the ${kingdomType}. Show don't tell, use examples and do not say "A recent economic event was...". Be specific with names of organizations, individuals, or locations',
+    impactful_economic_event: 'describe a recent event that either had a positive or negative impact on the economy of the ${kingdomType}. Be specific with names of organizations, individuals, or locations',
     military_history: 'A brief history of the military structure of the ${kingdomType}',
     recent_event_military: 'this sentence should tie into the previous sentence about military history. describe a recent military event which reflects the current state of the ${kingdomType}. Show don't tell, use examples and do not say "A recent military event was...". Be specific with names of organizations, individuals, or locations',
-    internal_threat_description: 'A description of an internal threat to the ${kingdomType}. This could be environmental, political, social, economic or magical in nature (if the ${kingdomType} has magic). Be specific with names of organizations, individuals, or locations',
-    external_threat_description: 'A description of an external threat to the ${kingdomType}. Be specific with names of organizations, individuals, or locations',
     greatest_hope: 'A description of the ${kingdomType}'s greatest hope. Be specific about what this hope is and why it is important',
     darkest_fear: 'A description of the ${kingdomType}'s darkest fear. Be specific about what this fear is and why it is important',
-  }
+    npc_list: [
+      {
+        name: 'NPC Name',
+        description: 'A brief description of the NPC's role and personality'
+      }
+      // Repeat the above structure for each NPC
+    ]
+  }`;
+}
+
+export function subLocationsPrompt(kingdomDescription) {
+  return `
+  Below is a description of a setting:
+  ${kingdomDescription}
+
+  Find all sub-locations mentioned in the above text and return a JSON array in the following format. Also, add 3-4 additional important sub-locations that would be found in the setting. Each sub-location should have a 'name' and 'description' key. Temperature: 0.9
+  [
+    {
+      "name": "Name of the sub-location",
+      "description": "Two sentences describing the sub-location. The first sentence should be a general overview of the location and the second sentence should provide some information about why the location is important or interesting."
+    }
+    // Repeat the above structure for each sub-location
+  ]
   `;
+}
+
+export function factionsPrompt(kingdomDescription) {
+  return `
+  Below is a description of a place in in JSON format:
+  ${kingdomDescription}
+
+  Based on the extensive background information provided about the above location, generate 4-5 faction profiles in JSON format. Each profile should include sections for 'history', 'recent_event', and 'current_situation'. Each section should provide a clear, coherent narrative from the faction's origins through to its current activities and influence. Please format the output as shown in the example below to ensure consistency across all entries.
+  Be sure that one of the factions is the ruling faction and another faction should be the arch rival of the ruling faction. Also consider including insurgent, downtrodden, religious, mystical, and outlier factions. Temperature: 0.9
+
+  Example JSON Structure:
+  [{
+      "name": "Example Faction",
+      "history": "Describe the origins and key historical developments of the faction.",
+      "recent_event": "Detail a significant event that recently impacted the faction. Be specific about event details including names, locations, and outcomes.",
+      "current_situation": "Outline the current goals, challenges, and influence of the faction. This sentence should tie into the previous sentence about the recent event. Be specific with names of organizations, individuals, or locations.",
+      "motto": "A short phrase that encapsulates the faction's core beliefs or goals.",
+      "faction_leader": "The name of the faction's current leader.",
+      "faction_leader_description": "A brief description of the faction leader's personality and leadership style. Always provide a name or at least a title or alias, even if it is unknown to most people",
+      "key_resources_and_assets": "What resources (like magical artifacts, strategic locations, skilled warriors) does the faction control? This can be a major factor in their strategies and conflicts.",
+      "rites_and_ceremonies": "Describe any rituals, ceremonies, or traditions that are important to the faction. These can be religious, cultural, or political in nature.",
+      "recent_ceremony": "Describe a recent celebration or ceremony held by the faction. What was the occasion and how did it reflect the faction's values or goals? Start with a transition like 'Recently,' or 'Last year...' and be specific about the event.",
+      "challenge_to_power": "What is the faction's biggest challenge to maintaining or expanding their power? This could be a rival faction, internal dissent, or a looming threat.",
+      "challenge_event": "Describe a recent event that exemplifies the faction's biggest challenge. Be specific about the event details and how it affects the faction's power or influence. This should tie into the previous sentence about the faction's challenge to power.",
+      "influence_level": "Provide a number from 1-10 that represents the faction's current influence in the kingdom. 1 is the lowest level of influence and 10 is the highest level of influence.",
+  }
+  // Repeat the above structure for each faction
+]`;
 }
